d6a164c776e850ba061d6360ebbb10aaa2e3a047
Gawrylak7 <77485148+Gawrylak7@users.noreply.github.com>
2022-02-09 09:28:55 +0100

    Add files via upload

---
+++ lab9/lab9/GradeAddWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab9
+{
+    /// <summary>
+    /// Logika interakcji dla klasy GradeAddWindow.xaml
+    /// </summary>
+    public partial class GradeAddWindow : Window
+    {
+        public Ocena ocena { get; set; }
+
+        public GradeAddWindow(Ocena ocena)
+        {
+            this.ocena = ocena;
+            InitializeComponent();
+            TextSubj.Text = ocena.przedmiot;
+            TextVal.Text = ocena.wartosc.ToString();
+        }
+
+        public GradeAddWindow() : this(new Ocena())
+        {
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!Regex.IsMatch(TextVal.Text, "\\d+((.?|,?)\\d+)?")) return;
+
+            ocena.przedmiot = TextSubj.Text;
+            ocena.wartosc = Convert.ToDouble(TextVal.Text);
+            DialogResult = true;
+        }
+    }
+}
---
+++ lab9/lab9/StudentWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using lab9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab9;
+
+public partial class StudentWindow : Window
+{
+    public Student student;
+    public StudentWindow()
+    {
+        InitializeComponent();
+    }
+
+    public StudentWindow(Student student = null) : this()
+    {
+        if (student != null)
+        {
+            nameBox.Text = student.imie;
+            surnameBox.Text = student.nazwisko;
+            albumNrBox.Text = student.NrIndeksu.ToString();
+            wydzialBox.Text = student.wydzial;
+        }
+
+        this.student = student ?? new Student();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (!Regex.IsMatch(nameBox.Text, "\\w+") || !Regex.IsMatch(surnameBox.Text, "\\w+") ||
+            !Regex.IsMatch(albumNrBox.Text, "\\d+") || !Regex.IsMatch(wydzialBox.Text, "\\w+"))
+        {
+            MessageBox.Show("Podano niepoprawne dane!");
+            return;
+        }
+        try
+        {
+            student.imie = nameBox.Text;
+            student.nazwisko = surnameBox.Text;
+            student.NrIndeksu = Convert.ToInt32(albumNrBox.Text);
+            student.wydzial = wydzialBox.Text;
+            DialogResult = true;
+        }
+        catch { }
+    }
+}
